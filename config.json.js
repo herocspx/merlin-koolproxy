b9d5b268567c3ec07dd2c9f0fd054185e07d4194
SouHong <4201217@qq.com>
2020-09-30 20:07:00 +0800

    Update for merlin 384

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2020-09-18_21:30", 
     "description": "去广告，没烦恼", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "46b483e03cd5e4f8865930d4ec0f3347", 
+    "md5": "6332b0f18281160cc944fb6afd9d4687", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.8.4.1"
